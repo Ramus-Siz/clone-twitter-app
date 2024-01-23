201bd68a909740aab0ed1b94a9d6bdd8a059f148
ramus <ramussiz29@gmail.com>
2024-01-23 04:09:49 +0100

    use tailwind: classe: tweet-content, tweet-body, img: tweet-avatar img

--- src/components/singleTweet.jsx
+++ src/components/singleTweet.jsx
@@ -4,16 +4,20 @@ import TweetActions from "./tweet-actions";
 export default function SingleTweet({ value }) {
   return (
     <div
-      className=" flex justify-start  items-start gap-4 px-6 py-6 border-b-1-[#2f3336]"
+      className=" flex justify-start items-start gap-4 px-6 py-6 border-b-1-[#2f3336]"
       key={value.id}
     >
       <div className="flex-[0_0_70px]">
         <Link to={`/profile/${value.username}`}>
-          <img src={value.userImageProfil} alt="" />
+          <img
+            src={value.userImageProfil}
+            alt=""
+            className="max-w-full max-h-100px rounded-full"
+          />
         </Link>
       </div>
-      <div className="tweet-content">
-        <div className="tweet-body">
+      <div className="flex flex-col items-start justify-start gap-7 w-full">
+        <div className="flex flex-col gap-2">
           <div className="tweet-title">
             <span className="tweet-title-author">{value.tweetTitle}</span>
             {value.isCertified && (
