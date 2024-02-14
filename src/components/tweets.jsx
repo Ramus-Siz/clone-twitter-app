5a6bc0588c873a79dabbbd83f5d2fd557e2310ee
ramus <ramussiz29@gmail.com>
2024-02-14 14:47:03 +0100

    update

--- src/components/tweets.jsx
+++ src/components/tweets.jsx
@@ -1,8 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import SingleTweet from "./singleTweet";
 import { tweetContext } from "../contexts";
-
+import jsondata from "../utils/initial-data.json";
 export default function Tweets() {
+  console.log(jsondata);
+  console.log(jsondata.currentUser.name);
+
   const { tweets } = useContext(tweetContext);
   return (
     <div className="tweets">
